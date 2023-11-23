b833e4bd292cf477e0b3e0abde2ffda9f2613823
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-22 21:41:59 -0600

    get users endpoint implemented

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from "express";
+import Usuario from "../models/usuario";
 
-export const getUsuarios=(req:Request,res:Response )=>{
+export const getUsuarios=async (req:Request,res:Response )=>{
+
+    const usuarios= await Usuario.findAll();
     
-    res.json({
-        msg:'getUsuarios'
-    })
+    res.json([usuarios]);
 }
 
-export const getUsuario=(req:Request,res:Response )=>{
+export const getUsuario=async (req:Request,res:Response )=>{
 
     const { id}= req.params;
+
+    const usuario= await Usuario.findByPk(id);
+
+    if(!usuario){
+        return res.status(404).json({
+            msg:`No existe el usuario con el id: ${id}`
+        })
+    }
     
     res.json({
-        msg:'getUsuario',
-        id
+        usuario
     })
 }
 
