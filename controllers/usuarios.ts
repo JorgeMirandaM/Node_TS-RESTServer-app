086545e24e3798f6d8c52351071e6ef4afb59fef
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-22 22:27:58 -0600

    create and update users function implemented

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -1,57 +1,126 @@
 import { Request, Response } from "express";
 import Usuario from "../models/usuario";
 
-export const getUsuarios=async (req:Request,res:Response )=>{
+export const getUsuarios = async (req: Request, res: Response) => {
 
-    const usuarios= await Usuario.findAll();
-    
-    res.json([usuarios]);
-}
+    try {
+        const usuarios = await Usuario.findAll();
 
-export const getUsuario=async (req:Request,res:Response )=>{
-
-    const { id}= req.params;
-
-    const usuario= await Usuario.findByPk(id);
-
-    if(!usuario){
-        return res.status(404).json({
-            msg:`No existe el usuario con el id: ${id}`
+        res.json([usuarios]);
+    } catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
         })
     }
-    
-    res.json({
-        usuario
-    })
 }
 
-export const postUsuario=(req:Request,res:Response )=>{
+export const getUsuario = async (req: Request, res: Response) => {
 
-    const { body}= req;
-    
-    res.json({
-        msg:'postUsuario',
-        body
-    })
+    try {
+        const { id } = req.params;
+
+        const usuario = await Usuario.findByPk(id);
+
+        if (!usuario) {
+            return res.status(404).json({
+                msg: `the user with the: ${id} does not exist`
+            })
+        }
+
+        res.json({
+            usuario
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
+        })
+    }
 }
 
-export const putUsuario=(req:Request,res:Response )=>{
+export const postUsuario = async (req: Request, res: Response) => {
 
-    const { id}= req.params;
-    const { body}= req;
-    
-    res.json({
-        msg:'putUsuario',
-        body
-    })
+    const { body } = req;
+
+    try {
+
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        })
+
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: `the user with email: ${body.email} already exists`
+            })
+        }
+
+        const { estado, ...data } = body;
+        const usuario = Usuario.build(data);
+        await usuario.save();
+
+        res.json(usuario);
+
+
+    } catch (error) {
+
+        res.status(500).json({
+            msg: 'there was an error on server'
+        })
+    }
 }
 
-export const deleteUsuario=(req:Request,res:Response )=>{
+export const putUsuario = async (req: Request, res: Response) => {
 
-    const { id}= req.params;
-    
+    const { id } = req.params;
+    const { body } = req;
+
+    try {
+
+        const usuario = await Usuario.findByPk(id);
+
+        if (!usuario) {
+            return res.status(404).json({
+                msg: `the user with id: ${id} doesn't exist`
+            })
+        }
+
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        })
+
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: `the user with email: ${body.email} already exists`
+            })
+        }
+
+        const { estado, ...data } = body;
+
+        await usuario.update(data);
+
+        res.json({
+            usuario
+        })
+
+
+
+    } catch (error) {
+
+        res.status(500).json({
+            msg: 'there was an error on server'
+        })
+    }
+}
+
+export const deleteUsuario = (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
     res.json({
-        msg:'deleteUsuario',
+        msg: 'deleteUsuario',
         id
     })
 }
--- dist/controllers/usuarios.js
+++ dist/controllers/usuarios.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,39 +26,94 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
 const usuario_1 = __importDefault(require("../models/usuario"));
 const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const usuarios = yield usuario_1.default.findAll();
-    res.json([usuarios]);
+    try {
+        const usuarios = yield usuario_1.default.findAll();
+        res.json([usuarios]);
+    }
+    catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
+        });
+    }
 });
 exports.getUsuarios = getUsuarios;
 const getUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id } = req.params;
-    const usuario = yield usuario_1.default.findByPk(id);
-    if (!usuario) {
-        return res.status(404).json({
-            msg: `No existe el usuario con el id: ${id}`
+    try {
+        const { id } = req.params;
+        const usuario = yield usuario_1.default.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({
+                msg: `the user with the: ${id} does not exist`
+            });
+        }
+        res.json({
+            usuario
         });
     }
-    res.json({
-        usuario
-    });
+    catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
+        });
+    }
 });
 exports.getUsuario = getUsuario;
-const postUsuario = (req, res) => {
+const postUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: 'postUsuario',
-        body
-    });
-};
+    try {
+        const existeEmail = yield usuario_1.default.findOne({
+            where: {
+                email: body.email
+            }
+        });
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: `the user with email: ${body.email} already exists`
+            });
+        }
+        const { estado } = body, data = __rest(body, ["estado"]);
+        const usuario = usuario_1.default.build(data);
+        yield usuario.save();
+        res.json(usuario);
+    }
+    catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
+        });
+    }
+});
 exports.postUsuario = postUsuario;
-const putUsuario = (req, res) => {
+const putUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: 'putUsuario',
-        body
-    });
-};
+    try {
+        const usuario = yield usuario_1.default.findByPk(id);
+        if (!usuario) {
+            return res.status(404).json({
+                msg: `the user with id: ${id} doesn't exist`
+            });
+        }
+        const existeEmail = yield usuario_1.default.findOne({
+            where: {
+                email: body.email
+            }
+        });
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: `the user with email: ${body.email} already exists`
+            });
+        }
+        const { estado } = body, data = __rest(body, ["estado"]);
+        yield usuario.update(data);
+        res.json({
+            usuario
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            msg: 'there was an error on server'
+        });
+    }
+});
 exports.putUsuario = putUsuario;
 const deleteUsuario = (req, res) => {
     const { id } = req.params;
